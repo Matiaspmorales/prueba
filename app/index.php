3237672554177bc969d7dc14883411a011db8f76
Matiaspmorales <matiaspmorales@outlook.com>
2026-08-15 23:22:25 -0400

    creación de index.php

---
+++ app/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config/database.php';
+
+require_once 'controllers/PacienteController.php';
+
+$controlador = new PacienteController($pdo);
+
+
+
+$accion = isset($_GET['accion']) ? $_GET['accion'] : 'listar';
+
+if ($accion === 'listar'){
+    $controlador->listar();
+} elseif ($accion === 'crear'){
+    $controlador->crear();
+}elseif ($accion === 'guardar'){
+    $controlador->guardar();
+} elseif ($accion === 'editar'){
+    $controlador->editar();
+} elseif ($accion === 'actualizar'){
+    $controlador->actualizar();
+}elseif ($accion === "eliminar") {
+    $controlador->eliminar();
+}else {
+    echo "Página no encontrada (Error 404).";
+}
+
+?>
